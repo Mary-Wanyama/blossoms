67bc5fdd9391ce50fdcee1ecba0687853709031e
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-06 00:09:36 +0300

    This commit adds fetched data to the home component. This are the initial blogs for the application

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,35 +1,35 @@
-// import React, { useState, useEffect} from "react";
+import React, { useState, useEffect} from "react";
 
-// function Home() {
-//     let url = 'http://localhost:8000/Blogs'
-//     const [data,setData ] = useState([])
+function Home() {
+    let url = 'http://localhost:8000/Blogs'
+    const [data,setData ] = useState([])
 
-//     useEffect(()=>{
-//         fetch(url)
-//         .then(res=> res.json())
-//         .then(json=>{
-//             setData(json)
-//             console.log(json)
-//         })
-//     },[])
+    useEffect(()=>{
+        fetch(url)
+        .then(res=> res.json())
+        .then(json=>{
+            setData(json)
+            console.log(json)
+        })
+    },[])
 
-// console.log(data)
+console.log(data)
 
-//     return(
-//         <div className="home">
+    return(
+        <div className="home">
 
-//             {/* <h2>Home page</h2> */} 
-//             {/* {data.map((blogs)=>( 
-//                 <div key={blogs.id}>
-//                 <h3>{blogs.title}</h3>
-//                 <p>{blogs.content}</p>
-//                 <em> ~ {blogs.author}</em>
-//                 <br/>
-//                 <button>{blogs.likes} likes</button>
-//                 </div>
+            {/* <h2>Home page</h2> */} 
+            {/* {data.map((blogs)=>( 
+                <div key={blogs.id}>
+                <h3>{blogs.title}</h3>
+                <p>{blogs.content}</p>
+                <em> ~ {blogs.author}</em>
+                <br/>
+                <button>{blogs.likes} likes</button>
+                </div>
 
-//             ))} */}
-//         </div>
-//     )
-// }
-// export default Home
+            ))} */}
+        </div>
+    )
+}
+export default Home
