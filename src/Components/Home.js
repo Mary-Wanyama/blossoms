ed8444aa69e174663ad917ce27f175c51f7e87c1
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-05 23:55:24 +0300

    this commit removes the initial json-server-template because it is inaccessible

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,61 +1,35 @@
-import React, { useState} from "react";
+// import React, { useState, useEffect} from "react";
 
-function Home() {
-    // let url = 'http://localhost:3000/'
-    // const [data,setData ] = useState([])
+// function Home() {
+//     let url = 'http://localhost:8000/Blogs'
+//     const [data,setData ] = useState([])
 
-    // useEffect(()=>{
-    //     fetch(url)
-    //     .then(res=> res.json())
-    //     .then(setData)
-    // },[])
+//     useEffect(()=>{
+//         fetch(url)
+//         .then(res=> res.json())
+//         .then(json=>{
+//             setData(json)
+//             console.log(json)
+//         })
+//     },[])
 
-    const [data, setData] = useState([{
-        id : 0,
-        title : 'Introduction to React',
-        content : 'React is javascript library that is confused for a framework',
-        author : 'Mary Wanyama',
-        likes : 0
-    },
-    {
-        id : 1,
-        title : 'Props and state in react',
-        content : 'Props and state are two feature of react that help in data transmission. they do a similar yet different task',
-        author : 'Immaculate Wanyama',
-        likes : 0
-    },
-    {
-        id : 2,
-        title : 'components in react',
-        content : 'Components are like the building blocks of react. ',
-        author : 'Mary Wanyama',
-        likes : 0
-    },
-    {
-        id : 3,
-        title : 'Main features of  React',
-        content : 'there are several main features in react. props, componts, hooks, lists and keys, and may others',
-        author : 'Mary Immaculate',
-        likes : 0
-    }
-])
+// console.log(data)
 
+//     return(
+//         <div className="home">
 
-    return(
-        <div className="home">
+//             {/* <h2>Home page</h2> */} 
+//             {/* {data.map((blogs)=>( 
+//                 <div key={blogs.id}>
+//                 <h3>{blogs.title}</h3>
+//                 <p>{blogs.content}</p>
+//                 <em> ~ {blogs.author}</em>
+//                 <br/>
+//                 <button>{blogs.likes} likes</button>
+//                 </div>
 
-            {/* <h2>Home page</h2> */} 
-            {data.map((blogs)=>( 
-                <div key={blogs.id}>
-                <h3>{blogs.title}</h3>
-                <p>{blogs.content}</p>
-                <em> ~ {blogs.author}</em>
-                <br/>
-                <button>{blogs.likes} likes</button>
-                </div>
-
-            ))}
-        </div>
-    )
-}
-export default Home
+//             ))} */}
+//         </div>
+//     )
+// }
+// export default Home
