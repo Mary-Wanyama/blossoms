b6aa2e8eb930965a7a7e7208a7f49c8044e3b145
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-06 21:33:39 +0300

    This commit adds styling to the about components making it visually appealing

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./About.css"
 
 
 function About() {
@@ -6,10 +7,12 @@ function About() {
 
 
     return (
-        <div>
+        <div className="div">
             <h3> Hi my name is Mary</h3>
-            <p> I am  a mecahinical engineer who is currently doing software engineering. I have currently worked with two companies one for mechanical engineering 
-                and another took a chance on me in there 
+            <p> I am  a mechanical engineer who is currently doing software engineering. I have currently worked with two companies one for mechanical engineering 
+                and another took a chance on me in there website development and am very grateful for that opportunity. I am passionate about engineering. I always feel like that there is something  
+                very pure about someone designing and bringing to life something. It is a glimpse of a persons soul. This blog is going
+                to provide some of the fact in engineering.
             </p> 
         </div>
     )
