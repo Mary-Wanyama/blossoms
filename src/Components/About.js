d78f9406a0a2551658f1ad2bf2163fa88de4a91f
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-07 09:27:48 +0300

    This commit combines all the css files of the components into one File.css. This will reduce unnecessary files in the repo

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./About.css"
+import "./File.css"
 
 
 function About() {
--- src/Components/Motivation.js
+++ src/Components/Motivation.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import './Motivation.css'
+
 
 function Motivation() {
 
