3e719d59f4597806697f547a2db6cbab74e1ba31
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-09 00:55:08 +0300

    This commit adds styling to the form inputs in the NewPost component

--- src/Components/NewPost.js
+++ src/Components/NewPost.js
@@ -41,12 +41,15 @@ function NewPost() {
             <h3>New Posts</h3>
             <br />
             <form  onSubmit={(e)=>addDataForm(e)}>
-                <input onChange={(e)=>handle(e)} type='text' id="title" value={data.title} required name="title" placeholder="title" />
-                <br /><br />
+                <p className="label">Title</p><input onChange={(e)=>handle(e)} type='text' id="title" value={data.title} required name="title" placeholder="title" />
+                
+                <p className="label">Content</p>
                 <input onChange={(e)=>handle(e)} type='text' id="content" value={data.content} required name="content" placeholder="enter your content"/>
-                <br /><br />
+                
+                <p className="label">Author</p>
+                
                 <input onChange={(e)=>handle(e)} type='text' id="author" value={data.author} required name="author" placeholder=" ~anonymous"/>
-                <br /><br />
+                
                 <input onChange={(e)=>handle(e)} type='submit'  className="submit"/>
             </form>
         </div>
