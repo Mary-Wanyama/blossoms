d41be699a86726a6dae2202319225ca001b9d83c
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-09 01:12:47 +0300

    This commit adds styling to the home page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,16 +8,14 @@ function Footer() {
         <footer>
             <span>
                 <h3>Blossoms</h3>
-                <p>Blossoms is a blog that is created for engineering fact contribution and reading. The readers can find some of the fun things that 
+                <p>Blossoms is a blog that is created for facts contribution and reading. The readers can find some of the fun things that 
                     are discussed here. it is created with transparency and therefore anyone can read and post their ideas in this platform
                 </p>
             </span>
             <span className="span"> </span>
             <span>
                 <h3>Terms and conditions</h3>
-                <p>I will update a full list of terms and conditions for using it. meanwhile keep the comments and posts respectful and 
-                    true
-                </p>
+                <p>keep the posts respectful and have fun</p>
             </span>
             
         </footer>
